ff008447b1d312d78cadd1de19c9dca6e4321156
mahmoud_shtayeh <mahmoud_shtayeh@mahshtayeh-pc>
2021-08-11 06:39:32 +0200

    Project Setup

---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/MainController.java
@@ -0,0 +1,14 @@
+package com.shopme.site;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="")
+public class MainController {
+	@GetMapping("")
+	public String viewHomePage() {
+		return "index"; 
+	}
+}
