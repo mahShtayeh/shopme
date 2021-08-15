42e942839f307ac7e23458c82fe3a302cbe5fe27
mahmoud_shtayeh <mahmoud_shtayeh@mahshtayeh-pc>
2021-08-15 12:11:28 +0200

    Create New User

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Controller
@@ -19,8 +21,30 @@ public class UserController {
 	@GetMapping("/users") 
 	public String listAll(Model model) {
 		List<User> listUsers = service.listUsers(); 
+		
 		model.addAttribute("listUsers", listUsers); 
 		
 		return "users"; 
+	}
+	
+	@GetMapping("/users/new") 
+	public String newUser(Model model) {
+		User user = new User();
+		List<Role> listRoles = service.listRoles(); 
+		
+		model.addAttribute("user", user); 
+		model.addAttribute("listRoles", listRoles); 
+		
+		return "user_form"; 
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		service.saveUser(user); 
+		
+		redirectAttributes.addFlashAttribute("message", 
+				"The user [" + user.getFirstName() + "] has been added successfully"); 
+		
+		return "redirect:/users"; 
 	}
 }
